1975a5f15d4709eee34f61aade6292ac8532ba85
marrodri <laza.mar64@gmail.com>
2023-10-07 12:46:49 -0700

    react icons added; navbar UI cleanup

--- src/components/navigationBar/navBar.jsx
+++ src/components/navigationBar/navBar.jsx
@@ -1,16 +1,29 @@
 // import {goBeaker} from '@chakra-ui/icons'
 import { useState } from "react";
+import { SlChemistry } from "react-icons/sl";
+import { GiChemicalTank } from "react-icons/gi";
+import NavBarButton from "../buttons/navBarButton.jsx"
 export default function NavBar() {
-  const [selectedTab, setSelectedTa] = useState();
+  const [selectedTab, setSelectedTab] = useState();
   return (
-    <div className="bg-red-400 w-[1080px] flex  justify-between items-center px-[40px]">
-      <div className="font-bold text-[32px] "> LAB LINK</div>
-
+    <div className="w-[1080px] h-[50px] bg-white flex  justify-between items-center px-[40px]">
+      <div className="flex">
+        <GiChemicalTank className="w-[50px] h-[50px]" />
+        {/* <SlChemistry className="w-[50px] h-[50px]"/> */}
+        <a
+          href=""
+          className="font-bold   text-[32px] hover:underline hover:text-black"
+        >
+          LAB LINK
+        </a>
+      </div>
+      <hr />
       {/* nav bar buttons */}
-      <div className="flex justify-evenly">
-        <div id="navBarButton">button 1</div>
-        <div id="navBarButton">button 2</div>
-        <div id="navBarButton">button 3</div>
+      <div className="flex justify-evenly space-x-3 h-full">
+        <NavBarButton text={"Home"} isSelected={false}/>
+        <NavBarButton text={"Blog"} isSelected={false}/>
+        <NavBarButton text={"About"} isSelected={false}/>
+        <NavBarButton text={"Contact"} isSelected={true}/>
       </div>
     </div>
   );
