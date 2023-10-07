ef3b46141d2eb091cbdfee45f541ae0c2c76bad7
marrodri <laza.mar64@gmail.com>
2023-10-07 14:49:03 -0700

    job position card UI polished

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,10 +2,10 @@ import NavBar from "../navigationBar/navBar.jsx";
 
 export default function Layout({ children }) {
   return (
-    <div className="bg-[#C5C5C5]">
+    <div className="bg-[#edebeb] w-auto">
       <NavBar />
       {/* for the smaller screen, it will have a smaller top-padding */}
-      <div className=" ">{children}</div>
+      <div className="w-full">{children}</div>
       {/* <Footer /> */}
     </div>
   );
--- src/pages/marketplace.jsx
+++ src/pages/marketplace.jsx
@@ -4,15 +4,15 @@ import JobPositionCard from "../components/cards/jobPositionCard.jsx";
 export default function MarketPlace() {
   //TODO bring the UI for the marketplace.
   return (
-    <div className="h-full w-full bg-[#C5C5C5] flex flex-col items-center">
+    <div className="h-full w-full bg-[#edebeb] py-[25px] flex flex-col items-center">
       {/* title */}
       <div className="flex flex-col items-start">
         <div className="font-bold text-[36px]">MARKETPLACE</div>
         {/* main body */}
         <div className="flex items-center justify-center flex-col space-y-2 w-auto h-auto">
           {/* search bar */}
-          <div className=" w-[600px] bg-green-300">
-            <Input placeholder="Search for positions ..." />
+          <div className=" w-[600px] bg-white rounded-md">
+            <Input placeholder="Search for Projects..." />
           </div>
           <div className="flex space-x-5">
             {/* filters */}
@@ -41,7 +41,7 @@ export default function MarketPlace() {
             <div className="flex flex-col items-start">
               <div className="text-[32px]">Results</div>
               <div className="flex flex-col space-y-3">
-                <JobPositionCard />
+                <JobPositionCard jobTitle="Software Engineer" companyName="Johnson Space Center"/>
                 <JobPositionCard />
                 <JobPositionCard />
                 <JobPositionCard />
