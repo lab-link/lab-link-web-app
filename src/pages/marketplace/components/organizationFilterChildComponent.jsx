b64b5b8cb10b52aca45451fb4d9209e46ac13055
marrodri <laza.mar64@gmail.com>
2023-10-07 16:20:07 -0700

    first checkbox created for the Organization label

--- src/pages/marketplace/components/organizationFilterChildComponent.jsx
+++ src/pages/marketplace/components/organizationFilterChildComponent.jsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
 
+export default function OrganizationFilterChildComponent() {
+  //todo: add checkbox.
+  const [selectedOrganizations, setSelectedOrganizations] = useState({});
 
-export default function OrganizationFilterChildComponent(){
-    return <div>organization filter child </div>
+  //TODO
+  useEffect( () => {
+    //TODO: fetch data from the api callback to set
+    //the available organizations to fetch and to filter out.
+    setSelectedOrganizations({ org1: false, org3: false, org2: false });
+  },[]);
+
+  const toggleSelectedOrganization = (organization) => {
+    const organizationBoolean = selectedOrganizations[organization];
+    setSelectedOrganizations({ organization: !organizationBoolean });
+  };
+
+  return (
+    <div className="flex flex-col">
+      {Object.entries(selectedOrganizations).forEach((key,value) => {
+        console.log(key);
+      })}
+      <Checkbox
+        label={"s"}
+        value={true}
+        onChange={() => {
+          toggleSelectedOrganization();
+        }}
+      />
+    </div>
+  );
+}
+
+function Checkbox({ label, value, onChange }) {
+  return (
+    <label>
+        <div>label</div>
+      <input type="checkbox" checked={value} onChange={onChange} />
+    </label>
+  );
 }
