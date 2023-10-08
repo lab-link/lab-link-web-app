ce9e3aa9be75f8671390dc4aa42d711e3bf947ab
marrodri <laza.mar64@gmail.com>
2023-10-07 21:45:51 -0700

    Organization page with About Us tab added

--- src/pages/organization/components/OrganizationBanner.jsx
+++ src/pages/organization/components/OrganizationBanner.jsx
@@ -1,9 +1,9 @@
-export default function OrganizationBanner() {
+export default function OrganizationBanner({avatarURL = null}) {
   return (
-    <div className="bg-red-400 w-[850px] h-[300px] rounded-2xl flex">
+    <div className="bg-gray-100 w-[850px] h-[300px] px-[40px] shadow-md rounded-lg flex items-center justify-between">
       {/*  */}
       <div className="text-[74px] ">NASA</div>
-      <div className="w-[100px] h-[100px] bg-green-300 rounded-[50px]">Avatar</div>
+      <div className="w-[200px] h-[200px] bg-green-300 rounded-[100px]">{!avatarURL ? <>avatar</>: avatarURL}</div>
     </div>
   );
 }
--- src/pages/organization/organization.jsx
+++ src/pages/organization/organization.jsx
@@ -1,12 +1,41 @@
 import OrganizationBanner from "./components/OrganizationBanner";
+import AboutUsOrganization from "./components/aboutUsOrganization";
 
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Box,
+  Stack,
+} from "@chakra-ui/react";
 
+export default function Organization() {
+  return (
+    <div className="flex flex-col h-[1000px] space-y-3">
+      Organization
+      <OrganizationBanner />
+      <Tabs align="center">
+        <TabList>
+          <Tab>About Us</Tab>
+          <Tab>Jobs</Tab>
+          <Tab>Milestones</Tab>
+        </TabList>
 
-export default function Organization(){
-    return <div className="flex flex-col">
-        Organization
+        <TabPanels>
+          <TabPanel>
+            <AboutUsOrganization />
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
 
-
-        <OrganizationBanner/>
-        </div>
+    </div>
+  );
 }
