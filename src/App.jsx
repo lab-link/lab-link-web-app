15cb5e70c8216f2939697138c0530507276e87d1
marrodri <laza.mar64@gmail.com>
2023-10-07 21:06:51 -0700

    organization banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/layout/NavBar.jsx";
+import Organization from "./pages/organization/organization";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="flex flex-col w-full h-full">
-      <NavBar/>      
+      {/* <NavBar/>       */}
+      <Organization/>
     </div>
   );
 }
