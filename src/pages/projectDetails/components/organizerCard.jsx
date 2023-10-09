60d351d8ca79a646c08dcbb2b920b2d9aa91cfd5
Joshua Magdaleno <joshua.j.magdaleno@gmail.com>
2023-10-08 23:24:52 -0700

    Revert "organization and marketplace dummy content" (#18)

--- src/pages/projectDetails/components/organizerCard.jsx
+++ src/pages/projectDetails/components/organizerCard.jsx
@@ -1,30 +1,16 @@
 import Linebreaker from "./linebreaker";
-import { faker } from "@faker-js/faker";
-import { Image } from "@chakra-ui/react";
-export default function OrganizerCard({
-  clientsName = "clientsName",
-  clientsPosition = "clientsPosition",
-}) {
+
+export default function OrganizerCard({clientsName="clientsName",clientsPosition="clientsPosition"}) {
   //with the id get the avatar, clientsName, and clientsPosition.
-  return (
+    return (
     <div className="flex flex-col items-start  w-[450px]">
       <div className="text-[32px]">Organizer</div>
-      <Linebreaker />
+      <Linebreaker/>
       <div className="flex my-[10px] space-x-3">
-        <div className="bg-gray-400 h-[80px] w-[80px] rounded-[50px]">
-          <Image
-            objectFit="cover"
-            borderRadius="full"
-            maxW={{ base: "25%", sm: "80px" }}
-            src={faker.image.avatar()}
-            alt={""}
-          />
-        </div>
+        <div className="bg-gray-400 h-[80px] w-[80px] rounded-[50px]">user</div>
         <div className="flex flex-col items-start">
-          <div className="text-[24px] font-semibold">{faker.person.fullName()}</div>
-          <div className="text-[24px] underline font-light">
-            {faker.person.jobTitle()}
-          </div>
+          <div className="text-[24px] font-semibold">{clientsName}</div>
+          <div className="text-[24px] underline font-light">{clientsPosition}</div>
         </div>
       </div>
     </div>
